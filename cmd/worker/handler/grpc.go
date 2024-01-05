5b36fc32d57df690e9e69d5343dbd28b820afa75
nasuwanachot <nabhan.suwanachote@agoda.com>
2024-01-05 21:09:59 +0700

    Update proto

--- cmd/worker/handler/grpc.go
+++ cmd/worker/handler/grpc.go
@@ -5,13 +5,11 @@ import (
 	"github.com/resource-aware-jds/resource-aware-jds/generated/proto/github.com/resource-aware-jds/resource-aware-jds/generated/proto"
 	"github.com/resource-aware-jds/resource-aware-jds/pkg/grpc"
 	"github.com/resource-aware-jds/resource-aware-jds/service"
-	"github.com/sirupsen/logrus"
 	"google.golang.org/protobuf/types/known/emptypb"
-	"strconv"
 )
 
 type GRPCHandler struct {
-	proto.UnimplementedComputeNodeServer
+	proto.UnimplementedWorkerNodeServer
 	workerService service.IWorker
 }
 
@@ -19,22 +17,11 @@ func ProvideWorkerGRPCHandler(grpcServer grpc.RAJDSGrpcServer, workerService ser
 	handler := GRPCHandler{
 		workerService: workerService,
 	}
-	proto.RegisterComputeNodeServer(grpcServer.GetGRPCServer(), &handler)
+	proto.RegisterWorkerNodeServer(grpcServer.GetGRPCServer(), &handler)
 	return handler
 }
 
-func (j *GRPCHandler) SendTask(context context.Context, task *proto.Task) (*emptypb.Empty, error) {
-	taskId := strconv.Itoa(int(task.TaskId))
-	err := j.workerService.SubmitTask(task.DockerImage, taskId, task.Input)
+func (j *GRPCHandler) SendTask(context context.Context, task *proto.RecievedTask) (*emptypb.Empty, error) {
+	err := j.workerService.SubmitTask(task.DockerImage, task.ID, task.TaskAttributes)
 	return &emptypb.Empty{}, err
-}
-
-func (j *GRPCHandler) ReportJob(context context.Context, report *proto.ReportTaskRequest) (*emptypb.Empty, error) {
-	if report.TotalJob == report.CurrentJob {
-		jobIdStr := strconv.Itoa(int(report.TaskId))
-		containerName := "rajds-" + jobIdStr
-		go j.workerService.RemoveContainer(containerName)
-	}
-	logrus.Info("Job id: ", report.TaskId, " Current: ", report.CurrentJob, " Total: ", report.TotalJob)
-	return &emptypb.Empty{}, nil
 }
