a5662d35146a6c5adbb7348ab76776c04c921fc3
Siratee K <sirateek@gmail.com>
2024-01-06 10:59:35 +0700

    feat: Prepare the proto and model

--- models/job.go
+++ models/job.go
@@ -11,23 +11,7 @@ const (
 )
 
 type Job struct {
-	ID         *primitive.ObjectID `bson:"_id,omitempty"`
-	Status     JobStatus           `bson:"status"`
-	Parameters map[string]any      `bson:"parameters"`
-	Tasks      []map[string]any    `bson:"distribution_parameters"`
-	ImageURL   string              `bson:"image_url"`
-	Logs       []JobLog            `bson:"logs"`
-}
-
-type JobLogType string
-
-const (
-	CreateJobLogType  JobLogType = "create"
-	DistributedToNode JobLogType = "distributed_to_node"
-)
-
-type JobLog struct {
-	ID     *primitive.ObjectID `bson:"_id,omitempty"`
-	Type   JobLogType          `bson:"type"`
-	NodeID string              `bson:"node_id,omitempty"`
+	ID       *primitive.ObjectID `bson:"_id,omitempty"`
+	Status   JobStatus           `bson:"status"`
+	ImageURL string              `bson:"image_url"`
 }
