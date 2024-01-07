873cea26782f3aa9844407b70167d5b036156818
nabhan-au <nabhan.au@gmail.com>
2024-01-07 23:31:39 +0700

    Add node registration

--- pkg/cert/certworker.go
+++ pkg/cert/certworker.go
@@ -1,5 +1,14 @@
 package cert
 
+import (
+	"context"
+	"crypto/rsa"
+	"crypto/x509"
+	"github.com/resource-aware-jds/resource-aware-jds/config"
+	"github.com/resource-aware-jds/resource-aware-jds/generated/proto/github.com/resource-aware-jds/resource-aware-jds/generated/proto"
+	"github.com/resource-aware-jds/resource-aware-jds/pkg/grpc"
+)
+
 type WorkerNodeCACertificate TLSCertificate
 
 type WorkerNodeCACertificateConfig struct {
@@ -20,17 +29,66 @@ type WorkerNodeTransportCertificate struct {
 	PrivateKeyFileLocation  string
 }
 
-func ProvideWorkerNodeTransportCertificate(config WorkerNodeTransportCertificate) (TransportCertificate, error) {
-	privateKeyData, err := LoadKeyFromFile(config.PrivateKeyFileLocation)
+func ProvideWorkerNodeTransportCertificate(workerCertificateConfig WorkerNodeTransportCertificate, controlPlaneConfig config.ControlPlaneConfigModel) (TransportCertificate, error) {
+	privateKeyData, err := LoadKeyFromFile(workerCertificateConfig.PrivateKeyFileLocation)
 	if err != nil {
-		// TODO: Call register worker node with CP
-		return nil, err
+		response, privateKeyData, err := registerWorker(workerCertificateConfig, controlPlaneConfig)
+		if err != nil {
+			return nil, err
+		}
+		certificate, err := LoadCertificate(response.Certificate)
+		if err != nil {
+			return nil, err
+		}
+		provideTLSCertificate, err := ProvideTLSCertificate(certificate, privateKeyData, false)
+		if err != nil {
+			return nil, err
+		}
+		err = provideTLSCertificate.SaveCertificateToFile(workerCertificateConfig.CertificateFileLocation, workerCertificateConfig.PrivateKeyFileLocation)
+		if err != nil {
+			return nil, err
+		}
+		return provideTLSCertificate, nil
 	}
 
-	certificateChain, err := LoadCertificatesFromFile(config.CertificateFileLocation)
+	certificateChain, err := LoadCertificatesFromFile(workerCertificateConfig.CertificateFileLocation)
 	if err != nil {
 		return nil, err
 	}
 
 	return ProvideTLSCertificate(certificateChain, privateKeyData, false)
+}
+
+func registerWorker(workerCertificateConfig WorkerNodeTransportCertificate, controlPlaneConfig config.ControlPlaneConfigModel) (*proto.ComputeNodeRegistrationResponse, KeyData, error) {
+	caCertificate, err := ProvideWorkerNodeCACertificate(WorkerNodeCACertificateConfig{
+		CACertificateFilePath: workerCertificateConfig.CertificateFileLocation,
+	})
+	if err != nil {
+		return nil, nil, err
+	}
+
+	grpcConn, err := grpc.ProvideRAJDSGrpcClient(grpc.ClientConfig{
+		Target:        controlPlaneConfig.GRPCServerAddress,
+		CACertificate: caCertificate,
+	})
+	if err != nil {
+		return nil, nil, err
+	}
+
+	controlPlaneClient := proto.NewControlPlaneClient(grpcConn.GetConnection())
+	publicKeyData, privateKeyData, err := GeneratePublicAndPrivateKeyPair()
+	if err != nil {
+		return nil, nil, err
+	}
+
+	result, err := controlPlaneClient.WorkerRegistration(context.Background(), &proto.ComputeNodeRegistrationRequest{
+		Ip:            "1234",
+		Port:          1234,
+		NodePublicKey: x509.MarshalPKCS1PublicKey(publicKeyData.GetRawKeyData().(*rsa.PublicKey)),
+	})
+	if err != nil {
+		return nil, nil, err
+	}
+
+	return result, privateKeyData, nil
 }
