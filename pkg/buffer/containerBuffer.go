600b289d469c927e1921a9b4b91ab874cdc2890f
nabhan-au <nabhan.au@gmail.com>
2024-01-13 18:03:46 +0700

    Add container buffer

--- pkg/buffer/containerBuffer.go
+++ pkg/buffer/containerBuffer.go
@@ -1,1 +1,14 @@
 package buffer
+
+import "github.com/resource-aware-jds/resource-aware-jds/models"
+
+type ContainerBuffer interface {
+	Store(id string, object *models.Container)
+	Pop(id string) *models.Container
+}
+
+func ProvideContainerBuffer() ContainerBuffer {
+	buffer := &Buffer[string, models.Container]{}
+	buffer.InitializeMap()
+	return buffer
+}
