f66cc04fa9bd1c3a9a5d7eb5bdbb25dc5b6a6509
Siratee K <sirateek@gmail.com>
2024-02-03 14:52:39 +0700

    fux: Add task un,arshalling tag

--- models/task.go
+++ models/task.go
@@ -13,12 +13,12 @@ const (
 )
 
 type Task struct {
-	ID             *primitive.ObjectID `bson:"_id,omitempty"`
-	Status         TaskStatus          `bson:"task_status"`
-	ImageUrl       string              `bson:"image_url"`
-	JobID          *primitive.ObjectID `bson:"job_id"`
-	TaskAttributes []byte              `bson:"task_attributes"`
-	Logs           []TaskLog           `bson:"logs,omitempty"`
+	ID             *primitive.ObjectID `bson:"_id,omitempty" json:"id"`
+	Status         TaskStatus          `bson:"task_status" json:"status" `
+	ImageUrl       string              `bson:"image_url" json:"imageURL"`
+	JobID          *primitive.ObjectID `bson:"job_id" json:"jobID"`
+	TaskAttributes []byte              `bson:"task_attributes" json:"taskAttributes"`
+	Logs           []TaskLog           `bson:"logs,omitempty" json:"logs"`
 }
 
 func (t *Task) DistributionSuccess(nodeID string) {
@@ -56,8 +56,8 @@ const (
 )
 
 type TaskLog struct {
-	Severity   LogSeverity       `bson:"severity"`
-	Parameters map[string]string `bson:"parameters"`
-	Message    string            `bson:"message"`
-	Timestamp  time.Time         `bson:"timestamp"`
+	Severity   LogSeverity       `bson:"severity" json:"severity"`
+	Parameters map[string]string `bson:"parameters" json:"parameters"`
+	Message    string            `bson:"message" json:"message"`
+	Timestamp  time.Time         `bson:"timestamp" json:"timestamp"`
 }
