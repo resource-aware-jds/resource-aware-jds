ae559c04c6a40b88dd978146a4458ef7b9dad6b2
Siratee K <siratee.k@lmwn.com>
2024-03-16 15:03:17 +0700

    fix: Update Resource aware distribuor to use base distributor

--- pkg/distribution/resourceawaredistributor.go
+++ pkg/distribution/resourceawaredistributor.go
@@ -4,28 +4,27 @@ import (
 	"context"
 	"errors"
 	"github.com/resource-aware-jds/resource-aware-jds/config"
-	"github.com/resource-aware-jds/resource-aware-jds/generated/proto/github.com/resource-aware-jds/resource-aware-jds/generated/proto"
 	"github.com/resource-aware-jds/resource-aware-jds/models"
 	"github.com/resource-aware-jds/resource-aware-jds/pkg/util"
 	"go.mongodb.org/mongo-driver/bson/primitive"
-	"go.opentelemetry.io/otel/attribute"
 	"go.opentelemetry.io/otel/metric"
 	"sort"
 )
 
+var (
+	ErrResourceAwareDistributorTaskDifferenceJob = errors.New("distribution failed due to jobID is not the same")
+)
+
 type ResourceAwareDistributor struct {
-	distributedTaskCounter metric.Int64Counter
-	config                 config.ResourceAwareDistributorConfigModel
+	baseDistributor
+
+	config config.ResourceAwareDistributorConfigModel
 }
 
 func ProvideResourceAwareDistributor(config config.ResourceAwareDistributorConfigModel, meter metric.Meter) Distributor {
-	counter, err := meter.Int64Counter("resource_aware_distributor_task")
-	if err != nil {
-		panic(err)
-	}
 	return &ResourceAwareDistributor{
-		distributedTaskCounter: counter,
-		config:                 config,
+		baseDistributor: newBaseDistributor(ResourceAwareDistributorName, meter),
+		config:          config,
 	}
 }
 
@@ -35,18 +34,9 @@ type maximumTaskForNode struct {
 }
 
 func (r ResourceAwareDistributor) Distribute(ctx context.Context, nodes []NodeMapper, tasks []models.Task, dependency DistributorDependency) (successTask []models.Task, distributionError []DistributeError, err error) {
-	// Expect that all the task should have the same job id
-	var jobID *primitive.ObjectID
-	for _, task := range tasks {
-		if jobID == nil {
-			jobID = task.JobID
-		} else if jobID != task.JobID {
-			return nil, nil, errors.New("[ResourceAwareDistributor] distribution failed due to jobID is not the same")
-		}
-	}
-
+	err = r.checkTaskWithSameJobID(tasks)
 	if err != nil {
-		return nil, nil, errors.New("[ResourceAwareDistributor] fail to get average resource usage")
+		return nil, nil, err
 	}
 
 	// Calculate maximum resource for each node that can take
@@ -68,39 +58,23 @@ func (r ResourceAwareDistributor) Distribute(ctx context.Context, nodes []NodeMa
 	for _, nodeWithMaximumTask := range nodeWithMaximumTasks {
 		toBeDistributedTasks := tasks[0:nodeWithMaximumTask.totalTask]
 		tasks = tasks[nodeWithMaximumTask.totalTask:]
-
 		for _, toBeDistributedTask := range toBeDistributedTasks {
-			err := r.distributeToNode(ctx, nodeWithMaximumTask.node, toBeDistributedTask)
-			if err != nil {
-				distributionError = append(distributionError, DistributeError{
-					Task:  toBeDistributedTask,
-					Error: err,
-				})
-				continue
-			}
-			successTask = append(successTask, toBeDistributedTask)
+			r.distributeToNode(ctx, nodeWithMaximumTask.node, toBeDistributedTask, &successTask, &distributionError)
 		}
 	}
 	return
 }
 
-func (r ResourceAwareDistributor) distributeToNode(ctx context.Context, node NodeMapper, task models.Task) error {
-	logger := node.Logger.WithField("taskID", task.ID.Hex())
-	logger.Info("[ResourceAwareDistributor] Sending task to the worker node")
-	_, err := node.GRPCConnection.SendTask(ctx, &proto.RecievedTask{
-		ID:             task.ID.Hex(),
-		TaskAttributes: task.TaskAttributes,
-		DockerImage:    task.ImageUrl,
-	})
-	if err != nil {
-		logger.Warnf("[ResourceAwareDistributor] Fail to distribute task to worker node (%s)", err.Error())
-		task.DistributionFailure(node.NodeEntry.NodeID, err)
-		return err
+func (r ResourceAwareDistributor) checkTaskWithSameJobID(tasks []models.Task) error {
+	// Expect that all the task should have the same job id
+	var jobID *primitive.ObjectID
+	for _, task := range tasks {
+		if jobID == nil {
+			jobID = task.JobID
+		} else if jobID != task.JobID {
+			return ErrResourceAwareDistributorTaskDifferenceJob
+		}
 	}
-	// Add log to success task
-	task.DistributionSuccess(node.NodeEntry.NodeID)
-	r.distributedTaskCounter.Add(ctx, 1, metric.WithAttributes(attribute.String("nodeID", node.NodeEntry.NodeID)))
-	logger.Info("[Distributor] Worker Node accepted the task")
 	return nil
 }
 
