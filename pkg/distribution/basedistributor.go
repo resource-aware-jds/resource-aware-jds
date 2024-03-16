a7b3a54f7d9f7ca941d44682feac56d39c6eff88
Siratee K <siratee.k@lmwn.com>
2024-03-16 14:55:00 +0700

    feat: Add base distributor

---
+++ pkg/distribution/basedistributor.go
@@ -0,0 +1,64 @@
+package distribution
+
+import (
+	"context"
+	"fmt"
+	"github.com/resource-aware-jds/resource-aware-jds/generated/proto/github.com/resource-aware-jds/resource-aware-jds/generated/proto"
+	"github.com/resource-aware-jds/resource-aware-jds/models"
+	"github.com/sirupsen/logrus"
+	"go.opentelemetry.io/otel/attribute"
+	"go.opentelemetry.io/otel/metric"
+)
+
+type baseDistributor struct {
+	logger        *logrus.Entry
+	metricCounter metric.Int64Counter
+}
+
+func newBaseDistributor(name string, meter metric.Meter) baseDistributor {
+	counter, _ := meter.Int64Counter(
+		fmt.Sprintf("rajds_%s_distributor_total_distribute_task", name),
+		metric.WithUnit("Task"),
+		metric.WithDescription(fmt.Sprintf("The total task(s) that has been distributed using %s distributor", name)),
+	)
+	logger := logrus.WithFields(logrus.Fields{
+		"role":             "Control Plane",
+		"component":        "distributor",
+		"distributor_name": name,
+	})
+
+	return baseDistributor{
+		metricCounter: counter,
+		logger:        logger,
+	}
+}
+
+func (b *baseDistributor) distributeToNode(ctx context.Context, node NodeMapper, task models.Task, successTask *[]models.Task, errorTask *[]DistributeError) {
+	logger := b.logger.WithFields(node.Logger.Data).WithField("taskID", task.ID.Hex())
+	logger.Info("Sending task to the worker node")
+	_, err := node.GRPCConnection.SendTask(ctx, &proto.RecievedTask{
+		ID:             task.ID.Hex(),
+		TaskAttributes: task.TaskAttributes,
+		DockerImage:    task.ImageUrl,
+	})
+
+	if err != nil {
+		logger.Warnf("Fail to distribute task to worker node (%s)", err.Error())
+		task.DistributionFailure(node.NodeEntry.NodeID, err)
+		*errorTask = append(*errorTask, DistributeError{
+			NodeEntry: node.NodeEntry,
+			Task:      task,
+			Error:     err,
+		})
+		return
+	}
+	// Add log to success task
+	task.DistributionSuccess(node.NodeEntry.NodeID)
+	b.metricCounter.Add(
+		ctx,
+		1,
+		metric.WithAttributes(attribute.String("nodeID", node.NodeEntry.NodeID)),
+	)
+	*successTask = append(*successTask, task)
+	logger.Info("Worker Node accepted the task")
+}
--- pkg/distribution/roundrobindistrobutor.go
+++ pkg/distribution/roundrobindistrobutor.go
@@ -2,24 +2,18 @@ package distribution
 
 import (
 	"context"
-	"github.com/resource-aware-jds/resource-aware-jds/generated/proto/github.com/resource-aware-jds/resource-aware-jds/generated/proto"
 	"github.com/resource-aware-jds/resource-aware-jds/models"
 	"github.com/resource-aware-jds/resource-aware-jds/pkg/datastructure"
-	"go.opentelemetry.io/otel/attribute"
 	"go.opentelemetry.io/otel/metric"
 )
 
 type RoundRobinDistributor struct {
-	distributedTaskCounter metric.Int64Counter
+	baseDistributor
 }
 
 func ProvideRoundRobinDistributor(meter metric.Meter) Distributor {
-	counter, err := meter.Int64Counter("cp_distributed_task")
-	if err != nil {
-		panic(err)
-	}
 	return &RoundRobinDistributor{
-		distributedTaskCounter: counter,
+		baseDistributor: newBaseDistributor("round_robin", meter),
 	}
 }
 
@@ -31,28 +25,7 @@ func (r RoundRobinDistributor) Distribute(ctx context.Context, nodes []NodeMappe
 
 	for _, task := range tasks {
 		focusedNode := nodeRoundRobin.Next()
-		logger := focusedNode.Logger.WithField("taskID", task.ID.Hex())
-		logger.Info("[Distributor] Sending task to the worker node")
-		_, err = focusedNode.GRPCConnection.SendTask(ctx, &proto.RecievedTask{
-			ID:             task.ID.Hex(),
-			TaskAttributes: task.TaskAttributes,
-			DockerImage:    task.ImageUrl,
-		})
-		if err != nil {
-			logger.Warnf("[Distributor] Fail to distribute task to worker node (%s)", err.Error())
-			task.DistributionFailure(focusedNode.NodeEntry.NodeID, err)
-			distributionError = append(distributionError, DistributeError{
-				NodeEntry: focusedNode.NodeEntry,
-				Task:      task,
-				Error:     err,
-			})
-			continue
-		}
-		// Add log to success task
-		task.DistributionSuccess(focusedNode.NodeEntry.NodeID)
-		successTask = append(successTask, task)
-		r.distributedTaskCounter.Add(ctx, 1, metric.WithAttributes(attribute.String("nodeID", focusedNode.NodeEntry.NodeID)))
-		logger.Info("[Distributor] Worker Node accepted the task")
+		r.distributeToNode(ctx, focusedNode, task, &successTask, &distributionError)
 	}
 	return successTask, distributionError, nil
 }
