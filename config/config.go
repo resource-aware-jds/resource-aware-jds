e5985ad3a71f77a727aee7cd7f970e46ef982073
Siratee K <sirateek@gmail.com>
2024-03-24 13:51:55 +0700

    fix: Update log formatter

--- config/config.go
+++ config/config.go
@@ -6,6 +6,9 @@ import (
 	"github.com/resource-aware-jds/resource-aware-jds/pkg/grpc"
 	"github.com/sirupsen/logrus"
 	"os"
+	"path"
+	"runtime"
+	"strconv"
 )
 
 type Config struct {
@@ -28,6 +31,28 @@ func ProvideConfig() (*Config, error) {
 
 	var config Config
 	envconfig.MustProcess("RAJDS", &config)
+
+	logrus.SetReportCaller(true)
+
+	var formatter logrus.Formatter
+
+	formatter = &logrus.JSONFormatter{
+		CallerPrettyfier: func(frame *runtime.Frame) (function string, file string) {
+			fileName := path.Base(frame.File) + ":" + strconv.Itoa(frame.Line)
+			return "", fileName
+		},
+	}
+	if config.DebugMode {
+		formatter = &logrus.TextFormatter{
+			CallerPrettyfier: func(frame *runtime.Frame) (function string, file string) {
+				fileName := path.Base(frame.File) + ":" + strconv.Itoa(frame.Line)
+				//return frame.Function, fileName
+				return "", fileName
+			},
+		}
+	}
+
+	logrus.SetFormatter(formatter)
 
 	return &config, nil
 }
